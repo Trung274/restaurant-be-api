836cce84a05e38dd2ce5e046a4b72160e3e95195
SSGHN\trungnt01 <trungnt01@unicloud.com.vn>
2026-03-03 14:47:54 +0700

    config for deploy

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -13,6 +13,12 @@ const options = {
       }
     },
     servers: [
+      ...(process.env.RENDER_EXTERNAL_URL
+        ? [{
+            url: process.env.RENDER_EXTERNAL_URL,
+            description: 'Production server'
+          }]
+        : []),
       {
         url: `http://localhost:${process.env.PORT || 5000}`,
         description: 'Development server'
